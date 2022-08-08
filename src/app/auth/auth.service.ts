a34eb5c285f571cc47a298152cb9ce109cdb01a5
Sayali Ahire <sayali.ahire@cognizant.com>
2022-08-08 15:20:00 +0530

    Completed login end to end

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable(
+{
+    providedIn: 'root'
+}
+)
+export class AuthService {
+
+    constructor(private http:HttpClient){}
+    signup(email : string, password : string)
+    {
+     return this.http.post('https://localhost:5001/controller/Register',
+        {
+            "userName": email,
+            "password": password
+        }
+        );
+    }
+    login(email : string, password : string)
+    {
+     return this.http.post('https://localhost:5001/controller/Login',
+        {
+            "userName": email,
+            "password": password
+        }
+        );
+    }
+}
