5ea0d9a3c63daba6d8dd697cb9217a5af234fc01
Sayali Ahire <sayali.ahire@cognizant.com>
2022-08-09 22:24:32 +0530

    Done with listing screen

--- src/app/loan/loan-list/loan-list.component.ts
+++ src/app/loan/loan-list/loan-list.component.ts
@@ -7,21 +7,44 @@ import { LoanService } from '../loan.service';
   styleUrls: ['./loan-list.component.css']
 })
 export class LoanListComponent implements OnInit {
-
-  
+  lstloan : any;
+  searchTerm : any;
+  lstloanTemp : any;
+  isadmin : false;
+  userId : number;
   constructor(private loanservice:LoanService) {
     
    }
 
   ngOnInit(): void {
-    let userId = localStorage.getItem("userId");
-    this.loanservice.loanlist(parseInt(userId)).subscribe(resData =>{
-      console.log(resData);
+    this.userId = parseInt(localStorage.getItem("userId"));
+    this.loanservice.loanlist(this.userId).subscribe((resData) => {
+      
+      this.lstloan = resData;
+      this.lstloanTemp = this.lstloan;
 
+      // assuming the role will come as string from backend like "admin"
+      // if user is admin then setting the isadmin flag.
+      // adding condition for just sample.
+      this.lstloan = (  this.lstloan || [])?.map((obj) => {
+        if(this.userId === 3002) {
+          return { ...obj, role: "admin"};
+        }
+        return {...obj, role: "user" };
+      });
+      console.log(this.lstloan);
     },
     error=>{
       console.log(error);
     })
   }
+  search(): void {
+    let term = this.searchTerm.toLowerCase();
+    this.lstloan = this.lstloanTemp.filter(function(tag) {
+        return ((tag.firstName.toLowerCase().indexOf(term) >= 0)
+                ||(tag.lastName.toLowerCase().indexOf(term)>=0)||
+                (tag.propertyAddress.toLowerCase().indexOf(term)>=0));
+    }); 
+}
 
 }
